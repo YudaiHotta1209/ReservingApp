7894e169b9b470bb8de8257f51e23f4db483ae8f
YudaiHotta1209 <yudai.app01@gmail.com>
2022-06-07 00:49:25 +0900

    6/26カレンダー修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
 CREATE SCHEMA IF NOT EXISTS `buildings`;
 USE `buildings`;
 
-集会室予約用
+
 CREATE TABLE IF NOT EXISTS meeting_info(
 　id bigint(50) NOT NULL AUTO_INCREMENT,
   roomid int(11) NOT NULL, 
   username VARCHAR(64),
   day varchar(20),
-  start VARCHAR,
-  close VARCHAR,
+  start TIMESTAMP.
+  end  TIMESTAMP,
   purpose VARCHAR(64),
   owner VARCHAR(64),
   people int(20),
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS meeting_info(
 );
 
 
-ログイン用
+
 CREATE TABLE IF NOT EXISTS users(
   username VARCHAR(64) NOT NULL primary key,
   password VARCHAR(128) NOT NULL
   authority enum('ADMIN','USER')not null
 );
 
-ユーザー情報詳細用
+
 CREATE TABLE IF NOT EXISTS user_info(
   username VARCHAR(64),
   address int,
